28f251ab1d90700bb6a645df8ab880069c943c42
ReadMyMemory <lame3925@gmail.com>
2024-02-12 16:20:19 +0900

    기본 Controller 생성

---
+++ src/main/java/com/example/springjwt/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.springjwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//Web 서버로 동작하는 게 아닌 API 서버로 동작. 따라서 Web 페이지를 리턴하는 게 아닌 객체 데이터나 스트링 데이터를 리턴할 것.
+@Controller
+@ResponseBody
+public class MainController {
+
+    @GetMapping("/")
+    public String mainP() {
+
+        return "Main Controller";
+    }
+}
